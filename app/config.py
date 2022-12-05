0bdd6786099aed3218d6610df406887f6a6d36bf
mhananssi <111750907+mhananssi@users.noreply.github.com>
2022-12-06 04:25:25 +0500

    setup celery monitoring tool

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config:
     VERIFY_EMAIL_URL = f'http://localhost:{env["APP_PORT"]}/api/v1/user/verify'
 
     CELERY_CONFIG = {
-        'broker_url': f'amqp://{env["RABBITMQ_USER"]}:{env["RABBITMQ_PASSWORD"]}@{env["RABBITMQ_CONTAINER"]}:{env["RABBITMQ_PORT"]}//',
+        'broker_url': f'amqp://{env["RABBITMQ_DEFAULT_USER"]}:{env["RABBITMQ_DEFAULT_PASS"]}@{env["RABBITMQ_CONTAINER"]}:{env["RABBITMQ_PORT"]}//',
         'result_backend': 'rpc://',
         'include': ['app.tasks.tasks'],
         'timezone': 'Asia/Karachi'
